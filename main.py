7667f9e90a3151fe1796628e89e9f2c63243b366
Jacob Fricano <jsf7393@rit.edu>
2023-11-30 17:50:09 -0500

    A lot of changes

--- main.py
+++ main.py
@@ -1,6 +1,85 @@
 import open3d as o3d
 import numpy as np
+from matplotlib import pyplot as plt
 import os
+import csv
+
+bb = dict()
+car_details = []
+prev_centers = dict()
+curr_centers = dict()
+
+
+def draw_bounding_boxes(vis, clusters, pcd):
+    global prev_centers
+    global curr_centers
+    global car_details
+    car_details = []
+    if len(curr_centers) > 0:
+        prev_centers = curr_centers.copy()
+    if len(bb) > 0:
+        vis.clear_geometries()
+    for i in range(0, 6):
+        details = []
+        details.append(i)
+        clust = np.asarray(clusters)
+        ind = np.where(clust == i)[0]
+        aabb = o3d.geometry.AxisAlignedBoundingBox()
+        aabb = aabb.create_from_points(points=pcd.select_by_index(ind).points)
+        aabb.color = (1, 0, 0)
+        bb[i] = aabb
+        curr_centers[i] = aabb.get_center()
+        details.append(aabb.get_center()[0])
+        details.append(aabb.get_center()[1])
+        details.append(aabb.get_center()[2])
+        vis.add_geometry(aabb)
+        if (len(prev_centers)) > 0:
+            vectors = calculate_vectors(i)
+            details.append(vectors[0])
+            details.append(vectors[1])
+            details.append(vectors[2])
+        else:
+            details.append(0)
+            details.append(0)
+            details.append(0)
+        min_bounds = aabb.get_min_bound()
+        max_bounds = aabb.get_max_bound()
+        details.append(min_bounds[0])
+        details.append(max_bounds[0])
+        details.append(min_bounds[1])
+        details.append(max_bounds[1])
+        details.append(min_bounds[2])
+        details.append(max_bounds[2])
+        car_details.append(details)
+
+
+def create_csv(file_num):
+    """
+    create a csv file for frame (called by other function (once per frame))
+    :param file_num: frame number
+    :return:
+    """
+    directory = "./perception_results"
+    file_name = "frame_" + str(file_num) + ".csv"
+    path = os.path.join(directory, file_name)
+    header = ['vehicle_id', 'position_x', 'position_y', 'position_z', 'mvec_x', 'mvec_y', 'mvec_z', 'bbox_x_min',
+              'bbox_x_max', 'bbox_y_min', 'bbox_y_max', 'bbox_z_min', 'bbox_z_max']
+    with open(path, 'w') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(header)
+        writer.writerows(car_details)
+
+
+def calculate_vectors(i):
+    prev_center = prev_centers.get(i)
+    curr_center = curr_centers.get(i)
+    vectors = []
+    if i in prev_centers.keys():
+        vectors.append(curr_center[0] - prev_center[0])
+        vectors.append(curr_center[1] - prev_center[1])
+        vectors.append(curr_center[2] - prev_center[2])
+        return vectors
+    return [0, 0, 0]
 
 
 def main():
@@ -14,18 +93,47 @@ def main():
     files = os.listdir(directory)
     files.sort(key=lambda x: int(os.path.splitext(x)[0]))
 
-    for i in range(1, len(files)):
+    for i in range(1, len(files), 1):
+        file_num = files[i].split(".")[0]
+
         prev = o3d.io.read_point_cloud(directory + files[i-1])
         tmp = o3d.io.read_point_cloud(directory + files[i])
+
         dist = tmp.compute_point_cloud_distance(prev)
         dist = np.asarray(dist)
-        ind = np.where(dist < 0.01)[0]
+        ind = np.where(dist > 0.001)[0]
         if first:
-            current = tmp.select_by_index(ind, invert=True)
+            current = tmp.select_by_index(ind)
+            current = current.remove_radius_outlier(nb_points=2, radius=1.5)[0]
+            # current = current.remove_statistical_outlier(nb_neighbors=2, std_ratio=2.0)[0]
+            clusters = np.array(current.cluster_dbscan(eps=2.0, min_points=2))
+            max_clust = 0
+            if clusters.size > 0:
+                max_clust = clusters.max()
+            print(f"{max_clust + 1} clusters")
+            colors = plt.get_cmap("tab20")(clusters / (max_clust if max_clust > 0 else 1))
+            colors[clusters < 0] = 0
+            current.colors = o3d.utility.Vector3dVector(colors[:, :3])
+            draw_bounding_boxes(vis, clusters, max_clust, current)
+            create_csv(file_num)
+
             vis.add_geometry(current)
             first = False
         else:
-            current.points = tmp.select_by_index(ind, invert=True).points
+            current.points = tmp.select_by_index(ind).points
+
+            current.points = current.remove_radius_outlier(nb_points=2, radius=1.5)[0].points
+            # current.points = current.remove_statistical_outlier(nb_neighbors=2, std_ratio=0.01)[0].points
+            clusters = np.array(current.cluster_dbscan(eps=2.0, min_points=2))
+            max_clust = 0
+            if clusters.size > 0:
+                max_clust = clusters.max()
+            print(f"{max_clust + 1} clusters")
+            colors = plt.get_cmap("tab20")(clusters / (max_clust if max_clust > 0 else 1))
+            colors[clusters < 0] = 0
+            current.colors = o3d.utility.Vector3dVector(colors[:, :3])
+            draw_bounding_boxes(vis, clusters, max_clust, current)
+            create_csv(file_num)
             vis.update_geometry(current)
             vis.poll_events()
             vis.update_renderer()
